1b35faad98a3a66f03274c9a9ae2d4cf3923be03
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-13 10:59:38 -0600

    js changes for FishHTML creator and some css changes to try and contain images and txt to their respective cards..  cards need CSS help

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -4,29 +4,30 @@
 
 // TODO: Import `useFish` from the data provider module
 import { useFish } from './FishDataProvider.js'
-import { Fish } from "./Fish.js"
+import { Fish } from './Fish.js'
 
 export const FishList = () => {
-
-    // Get a reference to the `<article class="content">` element
+    
     const contentElement = document.querySelector(".containerLeft__fishList")
     const fishes = useFish()
 
-    //  // Generate all of the HTML for all of the fish
-    //  let fishHTMLRepresentations = ""
-    //  for (const fish of fishes) {
-    //      /*
-    //          Invoke the Fish component function
-    //          and pass the current fish object as an argument.
-    //          Each time, add the return value to the
-    //          fishHTMLRepresentations variable with `+=`
-    //      */
-    //  }
+     // Generate all of the HTML for all of the fish
+     let fishHTMLRepresentations = ""
+     for (const fish of fishes) {
+        fishHTMLRepresentations += Fish(fish)
+         /*
+             Invoke the Fish component function
+             and pass the current fish object as an argument.
+             Each time, add the return value to the
+             fishHTMLRepresentations variable with `+=`
+         */
+     }
 
     // Add to the existing HTML in the content element
     contentElement.innerHTML += `
         <article class="fishList">
-            All the fish go here!
+            ${fishHTMLRepresentations}
         </article>
     `
 }
+
