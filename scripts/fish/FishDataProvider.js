fa5584b1e406941e67fd1a4fb1933752a245aed9
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-15 10:45:04 -0600

    added 3 holyFish functions, need completion

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -3,7 +3,7 @@ const fishCollection = [
         image: "images/nemo-2.jpg",
         name: "Nemo",
         species: "Clownfish",
-        length: "3-4 inches",
+        length: 3,
         location: "Anemonefish are endemic to the warmer waters of the Indian Ocean, including the Red Sea and Pacific Oceans, the Great Barrier Reef, Southeast Asia, Japan, and the Indo-Malaysian region.",
         food: "Anemonefish primarily feed on small zooplankton from the water column, such as copepods and tunicate larvae, with a small portion of their diet coming from algae.",
     },
@@ -11,7 +11,7 @@ const fishCollection = [
         image: "images/Nemo_Maybe.png",
         name: "Nemo Maybe",
         species: "Clownfish",
-        length: "3-4 inches",
+        length: 5,
         location: "Anemonefish are endemic to the warmer waters of the Indian Ocean, including the Red Sea and Pacific Oceans, the Great Barrier Reef, Southeast Asia, Japan, and the Indo-Malaysian region.",
         food: "Anemonefish primarily feed on small zooplankton from the water column, such as copepods and tunicate larvae, with a small portion of their diet coming from algae.",
     },
@@ -19,7 +19,7 @@ const fishCollection = [
         image: "images/blueMarlin.jpg",
         name: "Marlin",
         species: "Blue Marlin/Makaira nigricans",
-        length: "up to 210 pounds",
+        length: 211,
         location: "Atlantic Ocean",
         food: "The larvae feed upon a variety of zooplankton along with drifting fish eggs and other larvae. They progress to feeding on a wide range of fishes, particularly scombrids, such as mackerel and tuna, squid, and especially near oceanic islands and coral reefs, on juvenile inshore fish.",
     },
@@ -27,7 +27,7 @@ const fishCollection = [
         image: "images/nemo-2.jpg",
         name: "Nemo",
         species: "Clownfish",
-        length: "3-4 inches",
+        length: 7,
         location: "Anemonefish are endemic to the warmer waters of the Indian Ocean, including the Red Sea and Pacific Oceans, the Great Barrier Reef, Southeast Asia, Japan, and the Indo-Malaysian region.",
         food: "Anemonefish primarily feed on small zooplankton from the water column, such as copepods and tunicate larvae, with a small portion of their diet coming from algae.",
     },
@@ -35,7 +35,7 @@ const fishCollection = [
         image: "images/nemo-2.jpg",
         name: "Nemo",
         species: "Clownfish",
-        length: "3-4 inches",
+        length: 9,
         location: "Anemonefish are endemic to the warmer waters of the Indian Ocean, including the Red Sea and Pacific Oceans, the Great Barrier Reef, Southeast Asia, Japan, and the Indo-Malaysian region.",
         food: "Anemonefish primarily feed on small zooplankton from the water column, such as copepods and tunicate larvae, with a small portion of their diet coming from algae.",
     }
@@ -45,31 +45,45 @@ export const useFish = () => {
     return fishCollection.slice()
 }
 
-//Begin 3 functions for Holy fish Martin encountered in Tikal.
+// Begin 3 functions for Holy fish Martin encountered in Tikal.
 // export const mostHolyFish = () => {
-//     // 3, 6, 9, 12, etc... fish
 //     const holyFish = []
 
-//     for () {
-
+//     for (fish of fishCollection) {
+//         if (fish.length % 3 === 0) {
+//            if (holyFish.length === 0) {
+//             holyFish += fish.name
+//         } else {
+//             holyFish += ", " + fish.name
+//         }
 //     }
-
 //     return holyFish
 // }
 
 // export const soldierFish = () => {
-//     // 5, 10, 15, 20, 25, etc... fish
-//     return soldiers
+//     const soldiers = []
+
+//     for (fish of fishCollection) {
+//         if (fish.length % 5 === 0) {
+//            if (soldiers.length === 0) {
+//             soldiers += fish.name
+//         } else {
+//             soldiers += ", " + fish.name
+//         }
+//     }
+// return soldiers
 // }
 
 // export const nonHolyFish = () => {
-//     // Any fish not a multiple of 3 or 5
-//     return regularFish
+//     const regularFish = []
+
+//     for (fish of fishCollection) {
+//         if (fish.length % 5 != 0 && fish.length % 3 != 0)
+//         (regularFish.length === 0) {
+//             regularFish += fish.name
+//         } else {
+//             regularFish += ", " + fish.name
+//         }
 // }
-
-
-// for (const number of numbers) {
-//     if (number % 2 === 0) {
-//         console.log(number)
-//     }
+// return holyFish
 // }
