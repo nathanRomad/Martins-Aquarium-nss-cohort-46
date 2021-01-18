6026cc45bfff88ba27529ee6c1a6956e3f391053
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-18 13:42:39 -0600

    some card fixes, text does not overflow and added scroll feature

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -1,7 +1,7 @@
 export const Fish = (fish) => {
     return `
         <section class="fishCard">
-            <div><img  class="fish__image image--card" src="${fish.image}" /></div>
+            <div><img  class="fishCard__image image--card" src="${fish.image}" /></div>
             <div class="fishCard__name">${fish.name}</div>
             <div class="fishCard__species">${fish.species}</div>
             <div class="fishCard__length">${fish.length}</div>
